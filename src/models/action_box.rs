3b3dc2b9edf3fe2d2356e10e4632af0f6e7292e2
Henriquehnnm <hello.henrique@proton.me>
2026-04-19 17:22:39 -0300

    Add structs and License

--- src/models/action_box.rs
+++ src/models/action_box.rs
@@ -1,3 +1,14 @@
+use time::OffsetDateTime;
+use uuid::Uuid;
+
 pub struct ActionBox {
-    name: String,
+    pub id: Uuid,
+    pub name: String,
+    pub description: Option<String>,
+    pub content: String, // Markdown
+    pub durarion: OffsetDateTime,
+    pub created_at: OffsetDateTime,
+    pub finished_at: OffsetDateTime,
 }
+
+impl ActionBox {}
